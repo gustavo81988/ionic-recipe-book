35bf4d227eb10a7ef589a1abf209fffe5960e91e
Gustavo Ramírez <gustavo81988@gmail.com>
2018-07-25 11:01:44 -0500

    Removing Item from the Shopping List

--- src/pages/shopping-list/shopping-list.ts
+++ src/pages/shopping-list/shopping-list.ts
@@ -27,6 +27,11 @@ export class ShoppingListPage {
     this.loadItems();
   }
 
+  onCheckItem(index: number){
+    this.slService.removeItem(index);
+    this.loadItems();
+  }
+
   private loadItems(){
     this.listItems = this.slService.getItems();
   }
