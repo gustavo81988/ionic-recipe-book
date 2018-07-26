41daf4be79e3bcd2cab13180e81fc81757396221
Gustavo Ramírez <gustavo81988@gmail.com>
2018-07-25 20:58:00 -0500

    Creating the Reactive Form

--- src/pages/edit-recipe/edit-recipe.ts
+++ src/pages/edit-recipe/edit-recipe.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @IonicPage()
 @Component({
@@ -7,12 +8,21 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
   templateUrl: 'edit-recipe.html',
 })
 export class EditRecipePage implements OnInit{
-  mode: string = 'New';
+  mode: string  = 'New';
   selectOptions = ['Easy','Medium','Hard'];
+  recipeForm: FormGroup;
 
   constructor(public navCtrl: NavController, public navParams: NavParams) {}
   
   ngOnInit(){
     this.mode = this.navParams.get('mode');
+  }
+
+  private initializeForm(){
+    this.recipeForm = new FormGroup({
+      'title'       : new FormControl(null, Validators.required),
+      'description' : new FormControl(null, Validators.required),
+      'difficulty'  : new FormControl('Medium', Validators.required),
+    });
   }
 }
