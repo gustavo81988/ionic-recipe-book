eb334c06d2aa38c2bc07ca2b2743854d95c64c6c
Gustavo Ramírez <gustavo81988@gmail.com>
2018-07-28 18:36:24 -0500

    Creating the Signup Page

--- src/pages/signup/signup.ts
+++ src/pages/signup/signup.ts
@@ -1,12 +1,6 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
-
-/**
- * Generated class for the SignupPage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
+import { IonicPage } from 'ionic-angular';
+import { NgForm } from '@angular/forms';
 
 @IonicPage()
 @Component({
@@ -14,12 +8,7 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
   templateUrl: 'signup.html',
 })
 export class SignupPage {
-
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  onSignup(form: NgForm){
+    console.log(form.value);
   }
-
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad SignupPage');
-  }
-
 }
